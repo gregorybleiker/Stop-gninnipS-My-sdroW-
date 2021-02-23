1adf993059389df53475962e49599a34cf2ed642
Gregory Bleiker <gregorybleiker@gmail.com>
2021-02-23 08:34:30 +0000

    use haskell version of reverse

--- main.hs
+++ main.hs
@@ -4,13 +4,9 @@ splitAtBlank (x:xs) r
   | x == ' ' = (r, xs)
   | otherwise = splitAtBlank xs (r++[x])
 
-reverseWord :: String -> String
-reverseWord [] = []
-reverseWord (x:xs) = reverseWord xs ++ [x]
-
 conditionalReverse :: String -> String
 conditionalReverse x
-  | length x > 4 = reverseWord x
+  | length x > 4 = reverse x
   | otherwise = x
 
 spinWords :: String -> String
